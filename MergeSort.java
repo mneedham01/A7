05efe72d5f7624f950366dac6fcf1d797df6c9e4
mneedham01 <mneedham@smith.edu>
2023-11-07 21:01:32 -0500

    added visual testing

--- MergeSort.java
+++ MergeSort.java
@@ -1,88 +1,85 @@
 import java.util.ArrayDeque;
 import java.util.Collections;
 
+/** */
 public class MergeSort {
 
+  /** */
   public static CardPile merge(CardPile list1, CardPile list2) {
     CardPile merged = new CardPile();
     // while both lists are not empty
-    while (! list1.isEmpty() || ! list2.isEmpty()) {
+    while (! list1.isEmpty() && ! list2.isEmpty()) {
       // look at the first element in each list
       Card head1 = list1.getFirst();
       Card head2 = list2.getFirst();
-      // if positive, then head1 is smaller
-      // if smaller is negative, then head2 is smaller
-      Card smaller = head1.compareTo(head2) > 0 ? head1 : head2;
+      // if negative, then head1 is smaller
+      // if positive, then head2 is smaller
+      Card smaller = head1.compareTo(head2) < 0 ? list1.removeFirst() : list2.removeFirst();
       // take the smaller of the two, add it to merged list
       merged.addLast(smaller);
+    }
+    // determine which list is empty, add the rest of the other to the end of merged
+    if (list1.isEmpty()) {
+      merged.addAll(list2);
+    } else {
+      merged.addAll(list1);
     }
     return merged;
   }
 
+  /**  */
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
     // register the starting configuration with the recorder
     record.add(unsorted);
 
     ArrayDeque<CardPile> queue = new ArrayDeque<CardPile>();
     // put each element into own singleton CardPiles
-    for (Card element : unsorted) {
-      Card[] array = {element}
+    for (Card card : unsorted) {
+      Card[] array = {card};
       CardPile single = new CardPile(array, 2 ,2);
       // add all the piles to a queue
       queue.add(single);
     }
     // while more than one list remains on the queue
-    while (! queue.isEmpty()) {
+    while (queue.size() > 1) {
       // remove the first two lists, merge
-      CardPile list1 = queue.pop();
-      CardPile list2 = queue.pop();
+      CardPile list1 = queue.removeFirst();
+      System.out.println("list 1 = " + list1);
+      CardPile list2 = queue.removeFirst();
+      System.out.println("list 2 = " + list2);
+      CardPile merged = merge(list1, list2);
+      System.out.println("merged = " + merged);
       // put result at the end of the queue
-      queue.addLast(merge(list1, list2));
+      queue.add(merged);
+      System.out.println(queue.getLast());
       record.next();        // tell it this is a new step
       for (CardPile pile: queue) { // add all piles
         record.add(pile);
       }
     }
-
-  //     ## MergeSort
-  // - Begin by placing each element of `unsorted` into its own new singleton `CardPile` and add all those piles to a queue.
-  // - While more than one list remains on the queue:
-  //   - Remove the first two lists from the queue and merge them, preserving their sorted order.
-  //   - Put the result back at the end of the queue.
-
-
-  // _To merge two sorted lists into a single sorted list:_
-  //   - Look at the first element in each list.
-  //   - Take the smaller of the two off the front of its old list and put it at the end of a new (merged) list.
-  //   - Repeat this until both one of the old lists is empty, at which point you can append the remainder of the other original list to the new list.
-  //   - If the original lists were sorted, and you always take the smallest element available, then the resulting list will also be sorted.
-
-
-    // ***********************************************************
-    // Here is where you'll do the "work" of MergeSort:
-    //   - Use queue to store the intermediate piles
-    //   - Don't forget to register the new state with the
-    //     recorder after each merge step:
-    //        record.next();        // tell it this is a new step
-    //        for (CardPile pile: queue) { // add all piles
-    //           record.add(pile);
-    //        }
-    // ***********************************************************
-
     // return the sorted result here
     return queue.remove();
   }
 
+  /**  */
    public static void main(String[] args) {
-    // initialize deck of cards
-    CardPile unsorted = new CardPile(Card.newDeck(true), 2, 2);
-    System.out.println("unsorted: "+ unsorted);
-    // shuffle it
-    Collections.shuffle(unsorted);
-    // create a new record
-    SortRecorder record = new SortRecorder();
-    // sort it
-    CardPile sorted = sort(unsorted, record);
-    System.out.println("sorted: "+ sorted);
+    // set up a class to record and display the sorting results
+    SortRecorder recorder = new SortRecorder();
+
+    // set up the deck of cards
+    Card.loadImages(recorder);
+    CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+    // mix up the cards
+    Collections.shuffle(cards);
+
+    // in your program, this would be a call to a real sorting algorithm
+    cards = sort(cards, recorder);
+
+    // We can print out the (un)sorted result:
+    System.out.println(cards);
+
+    // make window appear showing the record
+    recorder.display("Card Sort Demo: MergeSort");
   }
 }
