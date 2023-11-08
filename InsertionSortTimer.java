d81a433dda6003a0b80d7944bb7a2a620b214a95
mneedham01 <mneedham@smith.edu>
2023-11-07 21:35:08 -0500

    edited indention

--- InsertionSortTimer.java
+++ InsertionSortTimer.java
@@ -26,18 +26,16 @@ public class InsertionSortTimer {
 
     /** Starts the program running */
     public static void main(String args[]) {
-        if (args.length<1) {
-        System.err.println("Please specify how many cards to sort!");
-        } else {
-        Card[] deck = Card.newDeck(true);
-        CardPile cards = new CardPile();
-
-        for (int i = 0; i<Integer.parseInt(args[0]); i++ ) {
-            cards.add(deck[(int)(52*Math.random())]);
-        }
-
-        sort(cards);
-    }
+      if (args.length<1) {
+          System.err.println("Please specify how many cards to sort!");
+      } else {
+          Card[] deck = Card.newDeck(true);
+          CardPile cards = new CardPile();
+          for (int i = 0; i<Integer.parseInt(args[0]); i++ ) {
+              cards.add(deck[(int)(52*Math.random())]);
+          }
+          sort(cards);
+      }
   }
 
 }
